31364a1cebde10807d8cc64c6b4465cdff68069e
edwardc_cit <edwardc@ciandt.com>
2020-08-24 00:03:58 +0800

    Update layout for welcome component.

--- src/components/welcome/welcome.vue
+++ src/components/welcome/welcome.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="welcome">
-    <div class="text-container">
-      <p class="title">Add Your</p>
-      <p class="text">Music</p>
+    <div class="welcome-top">
+      <div class="text-container">
+        <p class="title">Add Your</p>
+        <p class="text">Music</p>
+      </div>
     </div>
-    <div class="welcome-bg">
+    <div class="welcome-middle">
       <img src="./people.png" />
     </div>
-    <div class="btn-skip-wrapper">
-      <span class="btn btn-skip">skip</span>
+    <div class="welcome-bottom">
+      <div class="pagination">
+        <span
+          v-for="(id,index) in pagination"
+          class="dot"
+          :class="currentIndex === index ?'active' :''"
+          :key="index"
+        ></span>
+      </div>
+      <div class="btn-skip-wrapper">
+        <span class="btn btn-skip">skip</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+      pagination: [{ id: 1 }, { id: 2 }, { id: 3 }]
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import 'common/style/variable.scss';
+@import "common/style/variable.scss";
 
 .welcome {
   position: fixed;
@@ -30,22 +49,25 @@ export default {};
   flex-direction: column;
   justify-content: flex-end;
 
-  .text-container {
+  .welcome-top {
     flex: 0 0 50%;
-    .title {
-      margin-bottom: 10px;
-      font-size: 30px;
-      font-weight: 100;
-    }
 
-    .text {
-      font-size: 30px;
-      font-weight: bold;
-      color: $black;
+    .text-container {
+      .title {
+        margin-bottom: 10px;
+        font-size: 30px;
+        font-weight: 100;
+        color: $black;
+      }
+
+      .text {
+        font-size: 30px;
+        font-weight: bold;
+      }
     }
   }
 
-  .welcome-bg {
+  .welcome-middle {
     flex: 0 0 30%;
     img {
       display: block;
@@ -54,19 +76,44 @@ export default {};
     }
   }
 
-  .btn-skip-wrapper {
-    position: relative;
+  .welcome-bottom {
     flex: 1;
-  }
+    padding: 20px;
 
-  .btn-skip {
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    display: inline-block;
-    border: 1px solid #f0edea;
-    border-radius: 10px;
-    padding: 10px 20px;
+    .pagination {
+      margin-top: 23px;
+      transform: translateY(-50%);
+      float: left;
+
+      .dot {
+        margin: 0 4px;
+        display: inline-block;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.2);
+        &.active {
+          width: 20px;
+          border-radius: 5px;
+          background-color: $orange;
+        }
+      }
+    }
+
+    .btn-skip-wrapper {
+      float: right;
+    }
+
+    .btn-skip {
+      display: inline-block;
+      border: 1px solid #f0edea;
+      border-radius: 10px;
+      padding: 0 20px;
+      width: 80px;
+      height: 46px;
+      line-height: 46px;
+      text-align: center;
+    }
   }
 }
 </style>
