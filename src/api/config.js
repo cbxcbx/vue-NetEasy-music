9f5e55e084bc80b11f237f5a43af31ecc6507b5d
edwardc <edwardc@ciandt.com>
2021-02-24 16:39:43 +0800

    Update index html.

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const HOST = 'http://localhost:3000';
+export const HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://218.244.159.175';
 export const ERR_OK = 200;
